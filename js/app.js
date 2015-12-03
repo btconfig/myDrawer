2624ae904695880c2cad390ce12e4f241c889e67
ls <ls@R05400.(none)>
2015-12-03 18:21:52 +0800

    update codes

--- js/app.js
+++ js/app.js
@@ -3,12 +3,13 @@ var myDrawerApp = angular.module('myDrawerApp', ['ui.router']);
 /**
  * app service
  */
-myDrawerApp.service('drawerService', ['$rootScope', '$interval', function($rootScope,$interval) {
+myDrawerApp.service('drawerService', ['$rootScope', '$interval', function($rootScope, $interval) {
 	var serviceFunc = {};
 	
 	// service functions
 	serviceFunc.updateList = function() {
 		var listLen = plus.storage.getLength();
+		$rootScope.itemList = [];
 		for(var i = 0; i < listLen; i ++) {
 			var keyName = plus.storage.key(i);
 			var valueObj = plus.storage.getItem(keyName);
@@ -20,7 +21,6 @@ myDrawerApp.service('drawerService', ['$rootScope', '$interval', function($rootS
 	// service initialization
 	var stop = $interval(function() {
 		if(window.plus){
-			$rootScope.itemList = [];
 			serviceFunc.updateList();
 			$interval.cancel(stop);
 		}
@@ -30,56 +30,97 @@ myDrawerApp.service('drawerService', ['$rootScope', '$interval', function($rootS
 }]);
 
 /**
- * main controller
- */
-myDrawerApp.controller('mainCtrl', ['$scope', '$state', 'drawerService', function($scope,$state,drawerService) {
-	$scope.ifNewItemState = false;
-	$scope.addNewItem = function () {
-		$state.go('newItem');
-		$scope.ifNewItemState = true;
-	};
-	$scope.goTolistPage = function () {
-		$state.go('list');
-		$scope.ifNewItemState = false;
-	};
-}]);
-
-/**
  * list controller
  */
-myDrawerApp.controller('listCtrl',['$scope', 'drawerService',function($scope,drawerService) {
+myDrawerApp.controller('listCtrl',['$scope', '$rootScope', '$timeout', '$state', 'drawerService',function($scope, $rootScope, $timeout, $state, drawerService) {
+	$scope.goToNewItemPage = function () {
+		$state.go('newItem');
+	};
+	$scope.deleteItem = function(index) {
+//		plus.io.resolveLocalFileSystemURL($rootScope.itemList[index].picPath, function ( entry ) {
+//			entry.remove();
+//		});
+		plus.storage.removeItem($rootScope.itemList[index].date);
+		$rootScope.itemList.splice(index, 1);
+	};
+	$scope.previewImage = function(picPath) {
+		var url = "./templates/camera_image.html";
+		var w = plus.webview.create(url,url,{hardwareAccelerated:true,scrollIndicator:'none',scalable:true,bounce:"all"});
+		w.addEventListener( "loaded", function(){
+			w.evalJS( "loadMedia('"+picPath+"')" );		
+		}, false );
+		w.addEventListener( "close", function() {
+			w = null;
+		}, false );
+		w.show( "pop-in" );
+	};
 }]);
 
 /**
  * new item controller
  */
-myDrawerApp.controller('newItemCtrl', ['$scope', 'drawerService', function($scope, drawerService) {
+myDrawerApp.controller('newItemCtrl', ['$scope', '$rootScope', '$state', 'drawerService', function($scope, $rootScope, $state, drawerService) {
 	$scope.newItem = {
-		"description": ""
+		"description": "",
+		"picPath": ""
+	};
+	$scope.goTolistPage = function () {
+		$state.go('list');
+	};
+	$scope.getPicPath = function() {
+		if ($scope.newItem.picPath === "") {
+			return "./img/plus.png";			
+		}		
+		return $scope.newItem.picPath;		
+	};
+	$scope.galleryImg = function (){
+		// 从相册中选择图片
+    	plus.gallery.pick( function(path){
+    		var dateObj = new Date();
+			$scope.newItem.date = dateObj.getFullYear() + '-'
+							  	+ (dateObj.getMonth() + 1) + '-'
+							  	+ dateObj.getDate() + ' '
+							  	+ dateObj.getHours() + ':'
+							  	+ dateObj.getMinutes() + ':'
+							  	+ dateObj.getSeconds();
+			$scope.newItem.picPath = path;
+			$scope.$apply();
+    	}, {filter:"image"} );
+    	}, function ( e ) {    		
 	};
 	// take a photo
 	$scope.getImage = function() {
 		var cmr = plus.camera.getCamera();
-		cmr.captureImage( function ( p ) {
-			plus.io.resolveLocalFileSystemURL( p, function ( entry ) {
-				var dateObj = new Date();
-				$scope.newItem.date = dateObj.getFullYear() + '-'
-									  	+ (dateObj.getMonth() + 1) + '-'
-									  	+ dateObj.getDate() + ' '
-									  	+ dateObj.getHours() + ':'
-									  	+ dateObj.getMinutes() + ':'
-									  	+ dateObj.getSeconds();
-				$scope.newItem.picPath = entry.toLocalURL();
-				var keyName = $scope.newItem.date;
-				var value = JSON.stringify($scope.newItem);
-				plus.storage.setItem(keyName, value);
-				drawerService.updateList();
-			}, function ( e ) {
-				console.log( "读取拍照文件错误："+e.message );
-			} );
+		cmr.captureImage( function ( path ) {
+			plus.gallery.save( path );
+			var dateObj = new Date();
+			$scope.newItem.date = dateObj.getFullYear() + '-'
+							  	+ (dateObj.getMonth() + 1) + '-'
+							  	+ dateObj.getDate() + ' '
+							  	+ dateObj.getHours() + ':'
+							  	+ dateObj.getMinutes() + ':'
+							  	+ dateObj.getSeconds();
+			$scope.newItem.picPath = path;
+			$scope.$apply();
 		}, function ( e ) {
-			console.log( "失败："+e.message );
-		}, {filename:"_doc/camera/",index:1} );
+		}, {filename:"_doc/gallery/",index:1} );
+	};
+	$scope.save = function() {
+		if ($scope.newItem.picPath != "") {
+			var keyName = $scope.newItem.date;
+			var value = JSON.stringify($scope.newItem);
+			plus.storage.setItem(keyName, value);
+			$rootScope.itemList.push($scope.newItem);
+			$scope.goTolistPage();
+		}
+	};
+	$scope.cancel = function() {
+//		if ($scope.newItem.picPath != "") {
+//			plus.io.resolveLocalFileSystemURL($scope.newItem.picPath, function ( entry ) {
+//				entry.remove();
+//			});
+//		}
+		$scope.goTolistPage();
 	};
 }]);
 
